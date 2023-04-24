09d5a6220222af26e15a7f0b3ae1f997881f3034
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2023-04-25 09:54:44 +1000

    Default PHP file.

---
+++ embed-redirect.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Embed Redirects
+ *
+ * @package           EmbedRedirects
+ * @author            Peter Wilson
+ * @copyright         2023 Peter Wilson
+ * @license           MIT
+ *
+ * @wordpress-plugin
+ * Plugin Name: Embed Redirects
+ * Description: Allow links to third party websites in embeds via a redirect.
+ * Version: 1.0.0
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ * Author: Peter Wilson
+ * Author URI: https://peterwilson.cc
+ * License: MIT
+ * Text Domain: embed-redirects
+ */
+
+namespace PWCC\EmbedRedirects;
+
